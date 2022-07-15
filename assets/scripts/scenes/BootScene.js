b7f30ad384e6dc6d806a65b5d319b9afabd14cd9
andrey1303110 <andrey1303110@gmail.com>
2022-07-15 16:46:26 +0300

    add setEndpoints to BootScene

--- assets/scripts/scenes/BootScene.js
+++ assets/scripts/scenes/BootScene.js
@@ -3,6 +3,10 @@ class BootScene extends Phaser.Scene {
         super("Boot");
     }
 
+    init() {
+        setEndpoints();
+    }
+
     preload() {
         this.load.image('scene_bg', 'assets/sprites/background.png');
     }
