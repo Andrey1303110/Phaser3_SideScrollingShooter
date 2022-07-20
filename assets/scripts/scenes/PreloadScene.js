e30676a80e4a3c2fa9eddf3debcd49dde1a2e676
andrey1303110 <andrey1303110@gmail.com>
2022-07-20 12:16:17 +0300

    add sprite rocket

--- assets/scripts/scenes/PreloadScene.js
+++ assets/scripts/scenes/PreloadScene.js
@@ -7,6 +7,7 @@ class PreloadScene extends Phaser.Scene {
         this.load.atlas('dragon', 'assets/sprites/dragon.png', 'assets/sprites/dragon.json');
         this.load.atlas('enemy', 'assets/sprites/enemy.png', 'assets/sprites/enemy.json');
         this.load.image('fire', 'assets/sprites/fire.png');
+        this.load.image('rocket', 'assets/sprites/rocket.png');
     }
 
     create() {
