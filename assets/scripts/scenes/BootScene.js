caa29782d04440e48c19c33aef00f10fd1cdd043
andrey1303110 <andrey1303110@gmail.com>
2022-07-21 11:55:07 +0300

    delete unusable

--- assets/scripts/scenes/BootScene.js
+++ assets/scripts/scenes/BootScene.js
@@ -9,7 +9,6 @@ class BootScene extends Phaser.Scene {
 
     preload() {
         for (let i = 0; i <= 7; i++) {
-            console.log(`scene_bg_${i}`);
             this.load.image(`scene_bg_${i}`, `assets/sprites/background${i}.png`);
         }
     }
