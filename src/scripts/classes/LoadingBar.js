4b5d87d674aaed26b70ddeb05e98d3514623b3eb
Andrey1303110 <andrey1303110@gmail.com>
2023-12-03 16:37:33 +0200

    refactor loading bar

--- src/scripts/classes/LoadingBar.js
+++ src/scripts/classes/LoadingBar.js
@@ -1,46 +1,59 @@
+import { SCENE_NAMES } from "../constants";
 import { config, rgbToHex } from "/src/scripts/main";
+
+const INITIAL_COLORS = {
+    r: 210,
+    g: 0,
+    b: 25,
+}
+
+const MAX_VALUE_GREEN = 225;
+const MAX_VALUE_RED = 200;
+
+const BOX_COLOR = 0xe3e1da;
 
 export class LoadingBar {
     constructor (scene) {
         this.scene = scene;
-        this.colors = {
-            r: 210,
-            g: 0,
-            b: 25,
-        };
-        this.style = {
-            boxColor: 0xe3e1da,
+
+        this._colors = {};
+        this._colors.r = INITIAL_COLORS.r;
+        this._colors.g = INITIAL_COLORS.g;
+        this._colors.b = INITIAL_COLORS.b;
+
+        this._style = {
+            boxColor: BOX_COLOR,
             x: config.width/2 - (config.width * .5 / 2),
             y: config.height/2 + config.height * .25,
             width: config.width * .5,
             height: config.height * .05,
         };
 
-        this.progressBox = this.scene.add.graphics();
-        this.progressBar = this.scene.add.graphics();
+        this._progressBox = this.scene.add.graphics();
+        this._progressBar = this.scene.add.graphics();
 
-        this.showProgressBox();
-        this.setEvents();
+        this._showProgressBox();
+        this._setEvents();
     }
 
-    setEvents(){
-        this.scene.load.on('progress', this.showProgressBar, this);
-        this.scene.load.on('complete', ()=>{this.scene.scene.start('Levels')}, this);
+    _setEvents(){
+        this.scene.load.on('progress', this._showProgressBar, this);
+        this.scene.load.on('complete', () => this.scene.scene.start(SCENE_NAMES.levels), this);
     }
 
-    showProgressBox(){
-        this.progressBox
-            .fillStyle(this.style.boxColor)
-            .fillRect(this.style.x, this.style.y, this.style.width, this.style.height);
+    _showProgressBox(){
+        this._progressBox
+            .fillStyle(this._style.boxColor)
+            .fillRect(this._style.x, this._style.y, this._style.width, this._style.height);
     }
 
-    showProgressBar(value){
-        this.colors.r = 210 - value * 200;
-        this.colors.g = value * 225;
+    _showProgressBar(value){
+        this._colors.r = INITIAL_COLORS.r - value * MAX_VALUE_RED;
+        this._colors.g = value * MAX_VALUE_GREEN;
 
-        this.progressBar
+        this._progressBar
             .clear()
-            .fillStyle(rgbToHex(this.colors))
-            .fillRect(this.style.x, this.style.y, this.style.width * value, this.style.height);
+            .fillStyle(rgbToHex(this._colors))
+            .fillRect(this._style.x, this._style.y, this._style.width * value, this._style.height);
     }
 }
