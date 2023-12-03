add296be4f28f259f86522c1db160dc6a2c3efc1
Andrey1303110 <andrey1303110@gmail.com>
2023-12-03 15:24:36 +0200

    refactor cde

--- src/scripts/scenes/PreloadScene.js
+++ src/scripts/scenes/PreloadScene.js
@@ -1,3 +1,4 @@
+import { ATLASES_FILES, AUDIO_FILES, IMAGE_FILES } from "../constants";
 import { LoadingBar } from "/src/scripts/classes/LoadingBar";
 import { config } from "/src/scripts/main";
 
@@ -7,12 +8,13 @@ export class PreloadScene extends Phaser.Scene {
     }
 
     preload(){
-        this.createBG();
+        this._createBG();
         new LoadingBar(this);
-        this.preloadAssets();
+        this._preloadResources();
+        this._preloadOther()
     }
 
-    createBG() {
+    _createBG() {
         this.sceneBG = this.add.sprite(config.width / 2, config.height / 2, 'bg').setAlpha(.925).setOrigin(.5).setInteractive();
 
         const scaleX = this.cameras.main.width / this.sceneBG.width;
@@ -21,72 +23,15 @@ export class PreloadScene extends Phaser.Scene {
         this.sceneBG.setScale(scale).setScrollFactor(0);
     }
 
-    preloadAtalses(){
-        this.load.atlas('dragon', './assets/sprites/dragon.png', './assets/sprites/dragon.json');
-        this.load.atlas('jet', './assets/sprites/jet.png', './assets/sprites/jet.json');
-        this.load.atlas('strategic_jet', './assets/sprites/strategic_jet.png', './assets/sprites/strategic_jet.json');
-        this.load.atlas('helicopter', './assets/sprites/helicopter.png', './assets/sprites/helicopter.json');
-        this.load.atlas('boom', './assets/sprites/boom.png', './assets/sprites/boom.json');
-    }
-
-    preloadImages(){
-        for (let i = 1; i <= config.Levels.length; i++) {
-            this.load.image(`bg${i}`, `./assets/sprites/bg${i}.png`);
-        }
-        this.load.image('fire', './assets/sprites/fire.png');
-        this.load.image('flag', './assets/sprites/flag.png');
-        this.load.image('rocket', './assets/sprites/rocket.png');
-        this.load.image('missile', './assets/sprites/missile.png');
-        this.load.image('missile_2', './assets/sprites/missile_2.png');
-        this.load.image('button_campaign', './assets/sprites/button_campaign.png');
-        this.load.image('button_unlim', './assets/sprites/button_unlim.png');
-        this.load.image('button_upgrade', './assets/sprites/button_upgrade.png');
-        this.load.image('map', './assets/sprites/map.png');
-        this.load.image('battle', './assets/sprites/battle.png');
-        this.load.image('ruby', './assets/sprites/ruby.png');
-        this.load.image('flag', './assets/sprites/flag.png');
-        this.load.image('frame', './assets/sprites/frame.png');
-        this.load.image('stamp', './assets/sprites/stamp.png');
-        this.load.image('close', './assets/sprites/close.png');
-        this.load.image('return', './assets/sprites/return.png');
-        this.load.image('pause', './assets/sprites/pause.png');
-        this.load.image('pause_bg', './assets/sprites/pause_bg.png');
-        this.load.image('restart', './assets/sprites/restart.png');
-        this.load.image('play', './assets/sprites/play.png');
-        this.load.image('scale', './assets/sprites/scale.png');
-        this.load.image('reload', './assets/sprites/reload.png');
-        this.load.image('velocity', './assets/sprites/velocity.png');
-        this.load.image('progressBar', './assets/sprites/progressBar.png');
-        this.load.image('progressBarFill', './assets/sprites/progressBarFill.png');
-    }
-
-    preloadAudios(){
-        this.load.audio('select', './assets/sounds/select.wav');
-        this.load.audio('error', './assets/sounds/error.mp3');
-        this.load.audio('rocket_launch', './assets/sounds/rocket_launch.mp3');
-        this.load.audio('fire_launch', './assets/sounds/fire_launch.mp3');
-        this.load.audio('missile_launch', './assets/sounds/missile_launch.mp3');
-        this.load.audio('missile_2_launch', './assets/sounds/missile_2_launch.mp3');
-        this.load.audio('explosion_small', './assets/sounds/explosion_small.mp3');
-        this.load.audio('wings', './assets/sounds/wings.mp3');
-        this.load.audio('stamp', './assets/sounds/stamp.mp3');
-        this.load.audio('ready', './assets/sounds/ready.mp3');
-        this.load.audio('died', './assets/sounds/died.mp3');
-        this.load.audio('win', './assets/sounds/win.mp3');
-        this.load.audio('upgrade', './assets/sounds/upgrade.mp3');
-        this.load.audio('level_up', './assets/sounds/level_up.mp3');
-        this.load.audio('whoosh', './assets/sounds/whoosh.mp3');
-        this.load.audio('click', './assets/sounds/click.mp3');
-    }
-
-    preloadOther(){
+    _preloadOther(){
         this.load.plugin('rexvirtualjoystickplugin', './assets/plugins/rexvirtualjoystickplugin.min.js', true);
     }
 
-    preloadAssets() {
-        this.preloadAtalses();
-        this.preloadImages();
-        this.preloadAudios();
-        this.preloadOther();
+    _preloadResources() {
+        config.Levels.forEach(level => this.load.image(`bg${level.level}`, `./assets/sprites/bg${level.level}.png`));
+
+        ATLASES_FILES.forEach(name => this.load.atlas(name, `./assets/sprites/${name}.png`, `./assets/sprites/${name}.json`));
+        IMAGE_FILES.forEach(name => this.load.image(name, `./assets/sprites/${name}.png`));
+        AUDIO_FILES.forEach(name => this.load.audio(name, `./assets/sounds/${name}.mp3`));
     }
 }
