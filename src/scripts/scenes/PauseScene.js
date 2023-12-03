af36605ccad981c8e3048a762716bfebd7daad19
Andrey1303110 <andrey1303110@gmail.com>
2023-12-03 18:21:40 +0200

    refactor code

--- src/scripts/scenes/PauseScene.js
+++ src/scripts/scenes/PauseScene.js
@@ -11,12 +11,12 @@ export class PauseScene extends Phaser.Scene {
     }
 
     create() {
-        this.createBG();
-        this.createButtons();
-        this.toggleMenu();
+        this._createBG();
+        this._createButtons();
+        this._toggleMenu();
     }
 
-    createBG() {
+    _createBG() {
         this.black_bg = this.add.rectangle(config.width / 2, config.height / 2, config.width, config.height, '0x000000', 0);
         this.sceneBG = this.add.sprite(config.width / 2, config.height * -1, 'pause_bg').setOrigin(.5);
 
@@ -28,38 +28,36 @@ export class PauseScene extends Phaser.Scene {
         })
     }
 
-    createButtons(){
+    _createButtons(){
         this.play_button = this.add.sprite(this.sceneBG.x, this.sceneBG.y, 'play')
             .setScale(.8)
             .setAlpha(.85)
             .setOrigin(.5)
             .setInteractive()
-            .on('pointerdown', ()=>{this.toggleMenu('resume')}, this)
-            .on('pointerover', ()=>{this.play_button.setAlpha(1)})
-            .on('pointerout', ()=>{this.play_button.setAlpha(.85)});
+            .on('pointerdown', () => this._toggleMenu('resume'), this)
+            .on('pointerover', () => this.play_button.setAlpha(1))
+            .on('pointerout', () => this.play_button.setAlpha(.85));
 
         this.restart_button = this.add.sprite(this.sceneBG.x + this.sceneBG.displayWidth * .25, this.sceneBG.y, 'restart')
             .setScale(1.25)
             .setAlpha(.65)
             .setOrigin(.5)
             .setInteractive()
-            .on('pointerdown', ()=>{this.toggleMenu('restart')}, this)
-            .on('pointerover', ()=>{this.restart_button.setAlpha(1)})
-            .on('pointerout', ()=>{this.restart_button.setAlpha(.65)});
+            .on('pointerdown', () => this._toggleMenu('restart'), this)
+            .on('pointerover', () => this.restart_button.setAlpha(1))
+            .on('pointerout', () => this.restart_button.setAlpha(.65));
 
         this.return_button = this.add.sprite(this.sceneBG.x - this.sceneBG.displayWidth * .25, this.sceneBG.y, 'return')
             .setScale(1.25)
             .setAlpha(.65)
             .setOrigin(.5)
             .setInteractive()
-            .on('pointerdown', ()=>{
-                this.toggleMenu('return')
-            }, this)
-            .on('pointerover', ()=>{this.return_button.setAlpha(1)})
-            .on('pointerout', ()=>{this.return_button.setAlpha(.65)});
+            .on('pointerdown', () => this._toggleMenu('return'), this)
+            .on('pointerover', () => this.return_button.setAlpha(1))
+            .on('pointerout', () => this.return_button.setAlpha(.65));
     }
 
-    toggleMenu(command){
+    _toggleMenu(command){
         const tween_duration = 750;
         const y = this.sceneBG.y < 0 ? screenEndpoints.top + this.sceneBG.displayHeight / 2 : config.height * -1;
 
@@ -71,14 +69,14 @@ export class PauseScene extends Phaser.Scene {
             onComplete: ()=>{
                 switch (command) {
                     case 'resume':
-                        this.scene.resume('Game');
+                        this.scene.resume(SCENE_NAMES.game);
                     break;
                     case 'restart':
-                        this.scene.start('Game');
+                        this.scene.start(SCENE_NAMES.game);
                     break;
                     case 'return':
-                        this.scene.stop('Game');
-                        this.scene.launch('Map');
+                        this.scene.stop(SCENE_NAMES.game);
+                        this.scene.launch(SCENE_NAMES.campain);
                     break;
                 }
             }
