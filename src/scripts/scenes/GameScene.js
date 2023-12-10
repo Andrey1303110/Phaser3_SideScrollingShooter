bc4900e418b9998d69b1e095f508b5778810401f
Andrey1303110 <andrey1303110@gmail.com>
2023-12-10 13:48:20 +0200

    refactor code

--- src/scripts/scenes/GameScene.js
+++ src/scripts/scenes/GameScene.js
@@ -14,53 +14,65 @@ export class GameScene extends Phaser.Scene {
             localStorage.setItem('firstTimePlay', '0');
         }
         this.info = data;
-        this.currentLevelScene = this.info.level;
-        this.currentScore = 0;
-        this.black_bg = null;
+        this._currentLevelScene = this.info.level;
+        this._currentScore = 0;
+        this._black_bg = null;
     }
 
     create(data) {
-        this.createBG(data);
-        this.getMaxEnemyHeightFrame();
         this.cursors = this.input.keyboard.createCursorKeys();
-        this.createPlayer();
-        this.createEnemies();
-        this.createCompleteEvents();
-        this.addOverlap();
-        this.createScoreText();
-        this.createSounds();
-        this.addPauseButton();
-        this.addJoystick();
-        this.addFireButton();
+
+        this._createBG(data);
+        this._getMaxEnemyHeightFrame();
+        this._createPlayer();
+        this._createEnemies();
+        this._createCompleteEvents();
+        this._addOverlap();
+        this._createScoreText();
+        this._createSounds();
+        this._addPauseButton();
+        this._addMobileButtons();
+        this._addJoystick();
+        this._addFireButton();
         if (!this.info?.unlim) {
-            this.addProgressBar();
+            this._addProgressBar();
         }
     }
 
-    addJoystick(){
-        const alpha_cof = document.body.clientWidth > 1280 ? 0 : 1;
+    update() {
+        this._sceneBG.tilePositionX += this._sceneBG.width / 10000 * this.speed;
+        this._dumpJoyStickState();
+        this._player.move();
+        this._player.shooting();
+    }
 
+    _addMobileButtons() {
+        if (document.body.clientWidth > 1280) return;
+
+        this._addJoystick();
+        this._addFireButton();
+    }
+
+    _addJoystick(){
         this.joyStick = this.plugins.get('rexvirtualjoystickplugin').add(this, {
             x: screenEndpoints.left + config.joystick.radius + config.joystick.gap,
             y: screenEndpoints.bottom - config.joystick.radius - config.joystick.gap,
             radius: config.joystick.radius,
-            base: this.add.circle(0, 0, config.joystick.radius).setStrokeStyle(3.5, 0x1a65ac).setAlpha(.75 * alpha_cof),
-            thumb: this.add.circle(0, 0, config.joystick.radius/2, 0xcccccc).setAlpha(.5 * alpha_cof),
+            base: this.add.circle(0, 0, config.joystick.radius).setStrokeStyle(3.5, 0x1a65ac).setAlpha(.75),
+            thumb: this.add.circle(0, 0, config.joystick.radius/2, 0xcccccc).setAlpha(0.5),
             dir: '8dir',
         });
     }
 
-    dumpJoyStickState() {
+    _dumpJoyStickState() {
         if (this.joyStick) {
             this.cursorKeys = this.joyStick.createCursorKeys();
         }
     }
 
-    addFireButton(){
-        const alpha_cof = document.body.clientWidth > 1280 ? 0 : 1;
-
+    _addFireButton(){
         this.fireButton = this.add.sprite(screenEndpoints.right - config.joystick.radius - config.joystick.gap, this.joyStick.y, 'fire')
-                        .setAlpha(0.65 * alpha_cof)
+                        .setAlpha(0.65)
                         .setInteractive()
                         .setActive(false)
                         .on('pointerup', () => {
@@ -68,38 +80,31 @@ export class GameScene extends Phaser.Scene {
                         }, this)
                         .on('pointerdown', () => {
                             this.fireButton.active = true;
-                            this.player.shooting();
+                            this._player.shooting();
                         }, this);
     }
 
-    update() {
-        this.sceneBG.tilePositionX += this.sceneBG.width / 10000 * this.speed;
-        this.dumpJoyStickState();
-        this.player.move();
-        this.player.shooting();
-    }
-
-    createBG(data) {
+    _createBG(data) {
         const bg_image = data?.unlim ? `bg${Phaser.Math.Between(1, config.Levels.length)}` : `bg${data.level}`;
 
         const real_height = this.textures.list[bg_image].source[0].height;
         const scale = config.height/real_height;
 
-        this.speed = config.Levels[this.currentLevelScene-1].velocity;
+        this.speed = config.Levels[this._currentLevelScene-1].velocity;
 
         if (scale !== 1) {
             this.speed /= scale;
         }
 
-        this.sceneBG = this.add.tileSprite(0, 0, config.width, config.height, bg_image).setOrigin(0).setScale(scale).setAlpha(.65);
+        this._sceneBG = this.add.tileSprite(0, 0, config.width, config.height, bg_image).setOrigin(0).setScale(scale).setAlpha(.65);
     }
 
-    createScoreText() {
+    _createScoreText() {
         if (this.hiScoreText) {
             this.hiScoreText.destroy();
         }
 
-        this.scoreText = this.add.text(screenEndpoints.right - config.width * .01, screenEndpoints.top + config.width * .01, this.currentScore, {
+        this.scoreText = this.add.text(screenEndpoints.right - config.width * .01, screenEndpoints.top + config.width * .01, this._currentScore, {
             font: `${config.width * .03}px DishOut`,
             fill: '#EA0000',
         }).setOrigin(1, 0).setAlpha(.75);
@@ -112,15 +117,15 @@ export class GameScene extends Phaser.Scene {
         }
     }
 
-    createPlayer() {
-        this.player = new Player({ scene: this });
+    _createPlayer() {
+        this._player = new Player({ scene: this });
     }
 
-    createEnemies() {
-        this.enemies = new Enemies(this);
+    _createEnemies() {
+        this._enemies = new Enemies(this);
     }
 
-    createSounds() {
+    _createSounds() {
         if (this.sounds) {
             return;
         }
@@ -136,19 +141,19 @@ export class GameScene extends Phaser.Scene {
         };
     }
 
-    addOverlap() {
-        this.physics.add.overlap(this.player.fires, this.enemies, this.onOverlap, undefined, this);
-        this.physics.add.overlap(this.enemies.fires, this.player, this.onOverlap, undefined, this);
-        this.physics.add.overlap(this.player.fires, this.enemies.fires, this.onOverlap, undefined, this);
-        this.physics.add.overlap(this.player, this.enemies, this.onOverlap, undefined, this);
+    _addOverlap() {
+        this.physics.add.overlap(this._player.fires, this._enemies, this._onOverlap, undefined, this);
+        this.physics.add.overlap(this._enemies.fires, this._player, this._onOverlap, undefined, this);
+        this.physics.add.overlap(this._player.fires, this._enemies.fires, this._onOverlap, undefined, this);
+        this.physics.add.overlap(this._player, this._enemies, this._onOverlap, undefined, this);
     }
 
-    onOverlap(source, target) {
+    _onOverlap(source, target) {
         if (target.x > config.width + target.displayWidth / 2) {
             return;
         }
 
-        if (source !== this.player && target !== this.player) {
+        if (source !== this._player && target !== this._player) {
             if (!this.info?.unlim) {
                 let losses_name = target.texture.key;
                 if (target.texture.key === 'strategic_jet') {
@@ -160,16 +165,16 @@ export class GameScene extends Phaser.Scene {
                 localStorage.setItem(`losses_${losses_name}`, ++old_value);
             }
 
-            let reward = Number((target.reward * Math.pow(config.level.scoreCof, this.currentLevelScene - 1)).toFixed(0));
-            this.currentScore += reward;
+            let reward = Number((target.reward * Math.pow(config.level.scoreCof, this._currentLevelScene - 1)).toFixed(0));
+            this._currentScore += reward;
 
             if (!this.info?.unlim) {
                 const last_score = Number(config.totalScore);
                 localStorage.setItem('totalScore', last_score + reward);
                 config.totalScore = last_score + reward;
             }
-            this.scoreText.text = this.currentScore;
-            this.updateProgressBar();
+            this.scoreText.text = this._currentScore;
+            this._updateProgressBar();
         }
 
         Boom.generate(this, target.x, target.y);
@@ -179,35 +184,35 @@ export class GameScene extends Phaser.Scene {
         this.sounds.explosion_small.play();
     }
 
-    createCompleteEvents() {
-        this.player.emit('killed');
-        this.player.once('killed', this.onComplete, this);
-        this.events.once('enemies-killed', this.onComplete, this);
+    _createCompleteEvents() {
+        this._player.emit('killed');
+        this._player.once('killed', this._onComplete, this);
+        this.events.once('enemies-killed', this._onComplete, this);
     }
 
-    onComplete() {
-        if (this.black_bg) {
+    _onComplete() {
+        if (this._black_bg) {
             return;
         }
 
-        this.black_bg = this.add.rectangle(config.width / 2, config.height / 2, config.width, config.height, '0x000000', 0).setInteractive().setDepth(9999999);
-        let final_text = this.add.text(this.black_bg.x, this.black_bg.y, '', {
+        this._black_bg = this.add.rectangle(config.width / 2, config.height / 2, config.width, config.height, '0x000000', 0).setInteractive().setDepth(9999999);
+        let final_text = this.add.text(this._black_bg.x, this._black_bg.y, '', {
             font: `${config.width * .03}px DishOut`,
             fill: '#EA0000',
-        }).setOrigin(0.5).setAlpha(0).setDepth(this.black_bg.depth);
+        }).setOrigin(0.5).setAlpha(0).setDepth(this._black_bg.depth);
         this.game.sound.stopAll();
 
-        if (this.player.active) {
+        if (this._player.active) {
             this.sounds.win.play();
             final_text.text = "GLORY TO UKRAINE!";
 
-            if (this.info.hiScore < this.currentScore) {
+            if (this.info.hiScore < this._currentScore) {
                 let hiScores = localStorage.getItem('hiScores').split(',');
-                hiScores[this.currentLevelScene - 1] = this.currentScore;
+                hiScores[this._currentLevelScene - 1] = this._currentScore;
                 localStorage.setItem('hiScores', hiScores.join());
             }
 
-            if (config.currentLevelScene <= this.currentLevelScene) {
+            if (config.currentLevelScene <= this._currentLevelScene) {
                 config.currentLevelScene++;
                 localStorage.setItem('currentLevelScene', config.currentLevelScene);
             }
@@ -216,14 +221,14 @@ export class GameScene extends Phaser.Scene {
             this.sounds.died.play();
 
             if (this.info.unlim) {
-                if (localStorage.getItem('unlimHiScores') < this.currentScore) {
-                    localStorage.setItem('unlimHiScores', this.currentScore);
+                if (localStorage.getItem('unlimHiScores') < this._currentScore) {
+                    localStorage.setItem('unlimHiScores', this._currentScore);
                 }
             }
         }
 
         this.tweens.add({
-            targets: [this.black_bg, final_text],
+            targets: [this._black_bg, final_text],
             fillAlpha: 1,
             alpha: 1,
             scale: final_text.scale * 2,
@@ -231,21 +236,21 @@ export class GameScene extends Phaser.Scene {
             duration: this.sounds.died.duration * 1000 * .75,
             onComplete: () => {
                 if (this.info.unlim) {
-                    this.scene.start('Levels');
+                    this.scene.start(SCENE_NAMES.main);
                 } else {
-                    this.scene.start('Map');
+                    this.scene.start(SCENE_NAMES.campain);
                 }
                 this.scene.stop();
             }
         })
 
-        this.enemies.stopTimer();
-        this.enemies.children.entries.forEach(enemy => {
+        this._enemies.stopTimer();
+        this._enemies.children.entries.forEach(enemy => {
             enemy.stopTimer();
         });
     }
 
-    addPauseButton() {
+    _addPauseButton() {
         this.add.sprite(screenEndpoints.left + config.width * .015, screenEndpoints.top + config.width * .015, 'pause')
             .setAlpha(0.65)
             .setInteractive()
@@ -255,28 +260,28 @@ export class GameScene extends Phaser.Scene {
             }, this);
     }
 
-    addProgressBar(){
-        this.progressBar = this.add.sprite(config.width/2, screenEndpoints.top + config.width * .0225, 'progressBar')
+    _addProgressBar(){
+        this._progressBar = this.add.sprite(config.width/2, screenEndpoints.top + config.width * .0225, 'progressBar')
             .setAlpha(0.95);
-        this.progressBar.fillProgress = this.add.sprite(this.progressBar.x + this.progressBar.displayWidth * .1, this.progressBar.y + this.progressBar.displayHeight * .04, 'progressBarFill')
+        this._progressBar.fillProgress = this.add.sprite(this._progressBar.x + this._progressBar.displayWidth * .1, this._progressBar.y + this._progressBar.displayHeight * .04, 'progressBarFill')
             .setAlpha(0.95);
         
-        this.progressBar.levelText = this.add.text(this.progressBar.x - this.progressBar.displayWidth * .38, this.progressBar.y - this.progressBar.displayHeight * .035, config.currentLevelPlayer, {
-            font: `${this.progressBar.displayHeight * .545}px DishOut`,
+        this._progressBar.levelText = this.add.text(this._progressBar.x - this._progressBar.displayWidth * .38, this._progressBar.y - this._progressBar.displayHeight * .035, config.currentLevelPlayer, {
+            font: `${this._progressBar.displayHeight * .545}px DishOut`,
             fill: '#FFFFFF',
         }).setOrigin(0.5).setAlpha(0.75);
 
-        this.updateProgressBar();
+        this._updateProgressBar();
     }
 
-    updateProgressBar(){
+    _updateProgressBar(){
         if (this.info?.unlim) {
             return;
         }
         let score = {
-            start: this.getRequiredScoreOnLevel(config.currentLevelPlayer - 1),
-            end: this.getRequiredScoreOnLevel(config.currentLevelPlayer),
-            diff: this.getRequiredScoreOnLevel(config.currentLevelPlayer) - this.getRequiredScoreOnLevel(config.currentLevelPlayer - 1)
+            start: this._getRequiredScoreOnLevel(config.currentLevelPlayer - 1),
+            end: this._getRequiredScoreOnLevel(config.currentLevelPlayer),
+            diff: this._getRequiredScoreOnLevel(config.currentLevelPlayer) - this._getRequiredScoreOnLevel(config.currentLevelPlayer - 1)
         };
         let currentProgress = (config.totalScore - score.start)/score.diff;
         if (config.currentLevelPlayer < 2) {
@@ -284,21 +289,21 @@ export class GameScene extends Phaser.Scene {
         }
 
         if (currentProgress >= 1) {
-            this.increaseLevel();
+            this._increaseLevel();
             score = {
-                start: this.getRequiredScoreOnLevel(config.currentLevelPlayer - 1),
-                end: this.getRequiredScoreOnLevel(config.currentLevelPlayer),
-                diff: this.getRequiredScoreOnLevel(config.currentLevelPlayer) - this.getRequiredScoreOnLevel(config.currentLevelPlayer - 1)
+                start: this._getRequiredScoreOnLevel(config.currentLevelPlayer - 1),
+                end: this._getRequiredScoreOnLevel(config.currentLevelPlayer),
+                diff: this._getRequiredScoreOnLevel(config.currentLevelPlayer) - this._getRequiredScoreOnLevel(config.currentLevelPlayer - 1)
             };
             currentProgress = 1 - (-1 * (config.totalScore - score.end)/score.diff);
         }
 
-        this.progressBar.fillProgress.frame.cutWidth = this.progressBar.fillProgress.displayWidth * currentProgress;
-        this.progressBar.fillProgress.frame.updateUVs();
-        this.progressBar.text = config.currentLevelPlayer;
+        this._progressBar.fillProgress.frame.cutWidth = this._progressBar.fillProgress.displayWidth * currentProgress;
+        this._progressBar.fillProgress.frame.updateUVs();
+        this._progressBar.text = config.currentLevelPlayer;
     }
 
-    increaseLevel(){
+    _increaseLevel(){
         this.sounds.level_up.play({volume: .5});
         config.currentLevelPlayer++;
         localStorage.setItem('currentLevelPlayer', config.currentLevelPlayer);
@@ -311,21 +316,21 @@ export class GameScene extends Phaser.Scene {
         this.tweens.add({
             targets: levelTextLabel,
             alpha: 1,
-            x: this.progressBar.levelText.x,
-            y: this.progressBar.levelText.y,
-            scale: this.progressBar.levelText.displayWidth/levelTextLabel.displayWidth,
+            x: this._progressBar.levelText.x,
+            y: this._progressBar.levelText.y,
+            scale: this._progressBar.levelText.displayWidth/levelTextLabel.displayWidth,
             ease: 'Linear',
             duration: 500,
             onComplete: () => {
                 levelTextLabel.destroy();
-                this.progressBar.levelText.text = config.currentLevelPlayer;
+                this._progressBar.levelText.text = config.currentLevelPlayer;
             }
         });
 
-        this.increaseMoney();
+        this._increaseMoney();
     }
 
-    getRequiredScoreOnLevel(level){
+    _getRequiredScoreOnLevel(level){
         let result = 0;
         for (let i = 0; i < level; i++) {
             result += Number((config.level.score * Math.pow(config.level.levelCof, i)).toFixed(0));
@@ -333,12 +338,12 @@ export class GameScene extends Phaser.Scene {
         return result;
     }
 
-    increaseMoney(){
+    _increaseMoney(){
         ++config.money;
         localStorage.setItem('money', config.money);
     }
 
-    getMaxEnemyHeightFrame() {
+    _getMaxEnemyHeightFrame() {
         let max_frame_height = 0;
         const frames = this.textures.list.helicopter.frames;
 
