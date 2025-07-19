6abf89152b1cc1dbbd75de99ebf59c10b0510eb2
Andrey1303110 <andrey1303110@gmail.com>
2025-07-19 16:06:46 +0300

    add new audio

--- src/scripts/constants.js
+++ src/scripts/constants.js
@@ -21,7 +21,7 @@ export const ATLASES_FILES = ['dragon', 'jet', 'strategic_jet', 'helicopter', 'b
 export const IMAGE_FILES = ['fire', 'flag', 'rocket', 'missile', 'missile_2', 'button_campaign', 'button_unlim', 'button_upgrade', 'map', 'battle', 'ruby', 'flag', 'frame', 'stamp', 'close', 'return', 'pause', 'pause_bg', 'restart', 'play', 'scale', 'reload', 'velocity', 'progress_bar', 'progress_bar_fill', 'skobeeva', 'chief_commander', 'commander', 'president', 'next', 'life_icon', 'health_bar_empty', 'health_bar_fill', 'health'];
 
 // Only .mp3
-export const AUDIO_FILES = ['select', 'error', 'rocket_launch', 'fire_launch', 'missile_launch', 'missile_2_launch', 'explosion_small', 'wings', 'stamp', 'ready', 'died', 'win', 'upgrade', 'level_up', 'whoosh', 'whoosh_map', 'click', 'fire_effect'];
+export const AUDIO_FILES = ['select', 'error', 'rocket_launch', 'fire_launch', 'missile_launch', 'missile_2_launch', 'explosion_small', 'wings', 'stamp', 'ready', 'died', 'win', 'upgrade', 'level_up', 'whoosh', 'whoosh_map', 'click', 'fire_effect', 'campaign_complete_song'];
 
 export const DEPTH_LAYERS = {
     NONE: 0,
