037750132e93c482beea0fb8625f84ce5e286b8e
Andrey1303110 <andrey1303110@gmail.com>
2025-07-20 17:58:18 +0300

    refactor Fires Move

--- src/scripts/prefabs/Fire.js
+++ src/scripts/prefabs/Fire.js
@@ -1,4 +1,4 @@
-import { config } from '../main';
+import { config, delayInMSec } from '../main';
 import { MovableObject } from './MovableObject'
 
 export class Fire extends MovableObject {
@@ -22,17 +22,48 @@ export class Fire extends MovableObject {
         this.launch_sound.play({volume: 0.35});
     }
 
-    move(){
-        if (this.texture.key === 'missile' || this.texture.key === 'missile_2') {
-            this.body.setVelocityY(250);
-            this.body.setVelocityX(this.velocity/10);
-            setTimeout(() => {
-                this.body.setVelocityY(this.velocity/50);
+    async move(){
+        switch (this.texture.key) {
+            case 'rocket':
+                this._rocketMove();
+                break;
+            case 'missile':
+                this._missileMove();
+                break;
+            case 'missile_2':
+                this._missile2Move();
+                break;
+            default:
                 this.body.setVelocityX(this.velocity);
-            }, 175);
-        } else {
-            this.body.setVelocityX(this.velocity);
         }
+    }
+
+    async _rocketMove() {
+        const randomDelay = Phaser.Math.Between(500, 1000);
+        
+        while (!this.isDead()) {
+            const randomVelocityModifier = (Phaser.Math.Between(-20, 20) * 0.01);
+        
+            this.body.setVelocityX(this.velocity);
+            await delayInMSec(this.scene, randomDelay);
+            this.body.setVelocityY(this.velocity * randomVelocityModifier);
+        }
+    }
+
+    async _missileMove() {
+        this.body.setVelocityY(230);
+        this.body.setVelocityX(this.velocity * 0);
+        await delayInMSec(this.scene, 250);
+        this.body.setVelocityY(this.velocity * 0.035);
+        this.body.setVelocityX(this.velocity);
+    }
+
+    async _missile2Move() {
+        this.body.setVelocityY(280);
+        this.body.setVelocityX(this.velocity * 0.1);
+        await delayInMSec(this.scene, 250);
+        this.body.setVelocityY(this.velocity * 0.05);
+        this.body.setVelocityX(this.velocity);
     }
 
     reset() {
